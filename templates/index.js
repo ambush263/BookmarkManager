a7805d72b27ca836774465bbb67670ec29e652dc
Vedananda-19 <pvedananda@gmail.com>
2026-03-17 22:31:12 +0530

    Made Bookmarks Visible

--- templates/index.js
+++ templates/index.js
@@ -1,7 +1,26 @@
-let bookmarks = [];
 let linkBtn = document.querySelector(".addLinkButton");
 let addBookmark = () => {
-    let link = document.querySelector(".linkInput").value;
-    if(link) bookmarks.push(link);
+    let linkInput = document.querySelector(".linkInput");
+    let nameInput = document.querySelector(".nameInput");
+    if(linkInput){
+        let link = document.createElement("a");
+        link.innerText=linkInput.value;
+        link.href=linkInput.value;
+        link.target="_blank";
+        let tagName = document.createElement("span");
+        tagName.innerText=nameInput.value;
+        let newlink = document.createElement("li");
+        let links = document.querySelector("#links");
+        newlink.classList.add("bookmarkTab");
+        let colon = document.createElement("span");
+        colon.innerText = "     :     ";
+        newlink.append(tagName);
+        newlink.append(colon);
+        newlink.append(link);
+        links.append(newlink);
+
+        linkInput.value="";
+        nameInput.value="";
+    }
 }
 linkBtn.addEventListener("click",addBookmark);
